43cba35aa484e3512425827ca3bb96890e7db28e
WEILNG XI <vailydia@hotmail.com>
2017-06-19 14:34:52 -0700

    Initial Commit

---
+++ MyLocations/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  MyLocations
+//
+//  Created by Weiling Xi on 19/06/2017.
+//  Copyright © 2017 Weiling Xi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ MyLocations/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MyLocations
+//
+//  Created by Weiling Xi on 19/06/2017.
+//  Copyright © 2017 Weiling Xi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
